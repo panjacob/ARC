1c1b5189808582d34f9e1bde966e2dc8c25f2f69
panjacob <s19615@pjwstk.edu.pl>
2020-12-07 23:00:46 +0100

    notations

--- expression_parsing/main.py
+++ expression_parsing/main.py
@@ -2,4 +2,4 @@ from expression_parsing.notation import Notation
 
 notation = Notation()
 
-notation.in_to_pre('(a+b)*(c+d)')
+notation.in_to_pre('3+4*2/(1-5)^2')
--- expression_parsing/notation.py
+++ expression_parsing/notation.py
@@ -1,7 +1,53 @@
 class Notation:
-    @staticmethod
-    def in_to_pre(e):
-        bracket_start = e.find('(')
-        print(bracket_start)
-        if bracket_start >= 0:
-            if
+
+    def in_to_pre(self, equation):
+        equation = equation.replace(" ", "")
+        output = []
+        stack = []
+        last_priority = -1
+        connector = ""
+        for c in equation:
+            priority = self.calculate_priority(c)
+            if priority == -1:
+                connector += c
+            elif connector != '':
+                output.append(connector)
+                connector = ''
+
+            if priority == 0:
+                if c == '(':
+                    stack.append(c)
+                else:
+                    while (True):
+                        if len(stack) <= 0:
+                            break
+                        popped = stack.pop()
+                        print(popped, len(stack))
+                        if popped == '(':
+                            break
+                        else:
+                            output.append(popped)
+
+            elif priority > last_priority and priority > -1:
+                stack.append(c)
+                last_priority = priority
+            elif priority > -1:
+                if len(stack) > 0:
+                        popped = stack.pop()
+                        output.append(popped)
+                stack.append(c)
+                last_priority = priority
+
+            print('input: ', c, '   output: ', output, '   stack: ', stack, "   connector: ", connector)
+
+    def calculate_priority(self, char):
+        if char == '(' or char == ')':
+            return 0
+        elif char == '^':
+            return 3
+        elif char == '*' or char == '/':
+            return 2
+        elif char == '+' or char == '-':
+            return 1
+        else:
+            return -1
