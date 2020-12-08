87b386bf926fd8efc6e9727ddc4bca2ec96d9c75
panjacob <s19615@pjwstk.edu.pl>
2020-12-08 22:51:15 +0100

    notations

--- expression_parsing/main.py
+++ expression_parsing/main.py
@@ -2,4 +2,13 @@ from expression_parsing.notation import Notation
 
 notation = Notation()
 
-notation.in_to_pre('3+4*2/(1-5)^2')
+# in1 = '3+4*2/(1-5)^2'
+# pre1 = notation.in_to_pre(in1)
+# print(pre1)
+# in1_prim = notation.pre_to_in(pre1)
+# print(in1_prim, in1)
+
+# notation.in_to_pre('12 + a * (b * c + d / e)')
+
+notation.pre_to_post('+ A B')
+
--- expression_parsing/notation.py
+++ expression_parsing/notation.py
@@ -1,44 +1,46 @@
 class Notation:
 
     def in_to_pre(self, equation):
-        equation = equation.replace(" ", "")
+        equation = self.str_to_arr_infix(equation)
         output = []
         stack = []
         last_priority = -1
-        connector = ""
+
         for c in equation:
             priority = self.calculate_priority(c)
             if priority == -1:
-                connector += c
-            elif connector != '':
-                output.append(connector)
-                connector = ''
-
-            if priority == 0:
+                output.append(c)
+            elif priority == 0:
                 if c == '(':
                     stack.append(c)
+                    last_priority = priority
                 else:
                     while (True):
                         if len(stack) <= 0:
                             break
                         popped = stack.pop()
-                        print(popped, len(stack))
                         if popped == '(':
                             break
                         else:
                             output.append(popped)
-
-            elif priority > last_priority and priority > -1:
+            elif priority > last_priority:
                 stack.append(c)
                 last_priority = priority
-            elif priority > -1:
+            else:
                 if len(stack) > 0:
-                        popped = stack.pop()
-                        output.append(popped)
+                    popped = stack.pop()
+                    output.append(popped)
                 stack.append(c)
                 last_priority = priority
+            # print('input: ', c, '   output: ', output, '   stack: ', stack)
+        while (True):
+            if len(stack) <= 0:
+                break
+            else:
+                output.append(stack.pop())
 
-            print('input: ', c, '   output: ', output, '   stack: ', stack, "   connector: ", connector)
+        result = ' '.join(map(str, output))
+        return result
 
     def calculate_priority(self, char):
         if char == '(' or char == ')':
@@ -51,3 +53,61 @@ class Notation:
             return 1
         else:
             return -1
+
+    def str_to_arr_infix(self, string):
+        string = string.replace(" ", "")
+        connector = ""
+        result = []
+        for x in string:
+            if self.calculate_priority(x) == -1:
+                connector += x
+            else:
+                if len(connector) > 0:
+                    result.append(connector)
+                    connector = ""
+                result.append(x)
+        if len(connector) > 0:
+            result.append(connector)
+        return result
+
+    def str_to_arr_postfix(self, string):
+        return string.split(" ")
+
+    def isOpperand(self, string):
+        opperands = ['*', '/', '+', '-', '^', '(', ')']
+        return string not in opperands
+
+    def pre_to_in(self, equation):
+        equation = self.str_to_arr_postfix(equation)
+        output = []
+        for c in equation:
+            if self.isOpperand(c):
+                output.insert(0, c)
+            else:
+                op1 = output[0]
+                output.pop(0)
+                op2 = output[0]
+                output.pop(0)
+                output.insert(0, "(" + op2 + c + op1 + ")")
+        return output
+
+    def pre_to_post(self, equation):
+        equation = self.str_to_arr_postfix(equation)
+        stack = []
+        i = len(equation) - 1
+        while (True):
+
+            c = equation[i]
+            print('stack: ', stack, '    equation: ', equation, '  c: ', c, '  i: ', i, 'len: ', len(equation))
+            if not self.isOpperand(c):
+                i -= 2
+                operator1 = equation.pop()
+                operator2 = equation.pop()
+                opperand = stack.pop()
+                print(operator1, operator2, opperand)
+                stack.append(operator1)
+                stack.append(operator2)
+                stack.append(opperand)
+            else:
+                stack.append(equation.pop())
+                i -= 1
